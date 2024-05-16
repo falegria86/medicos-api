85defad635a40e1fcc707bcade0e0de4a95ab97f
fgalegria <francisco.gomez@comsys.mx>
2024-05-16 09:47:22 -0700

    Fixed env port

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import routes from './routes';
+import 'dotenv/config';
 
 const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-const port = 8080;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
